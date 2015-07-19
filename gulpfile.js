062278871cc63115445aa42484e80c940c1656c5
John Tasto <john@tasto.net>
2015-07-19 03:24:49 -0700

    Added CSS minification and autoprefixing

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var   gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-      sass = require('gulp-sass'),
-sourcemaps = require('gulp-sourcemaps'),
-       del = require('del');
+var     gulp = require('gulp'),
+      concat = require('gulp-concat'),
+      uglify = require('gulp-uglify'),
+      rename = require('gulp-rename'),
+        sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+   minifycss = require('gulp-minify-css'),
+  sourcemaps = require('gulp-sourcemaps'),
+         del = require('del');
 
 gulp.task('concatScripts', function() {
   return gulp.src(['js/main.js'])
@@ -27,10 +29,18 @@ gulp.task('compileSass', function() {
   return gulp.src('scss/main.scss')
              .pipe(sourcemaps.init())
              .pipe(sass())
+             .pipe(autoprefixer(['last 2 versions', 'safari 5', 'ie6', 'ie7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']))
              .pipe(rename('app.css'))
              .pipe(sourcemaps.write('./'))
              .pipe(gulp.dest('css'));
 });
+
+gulp.task('minifyCSS', ['compileSass'], function() {
+  return gulp.src('css/app.css')
+             .pipe(minifycss())
+             .pipe(rename('app.min.css'))
+             .pipe(gulp.dest('css'));
+})
 
 gulp.task('watch', function() {
   gulp.watch('scss/**/*.scss', ['compileSass']);
@@ -38,10 +48,10 @@ gulp.task('watch', function() {
 });
 
 gulp.task('clean', function() {
-  del(['dist', 'css/app.css*', 'js/app*.js*']);
+  del(['dist', 'css/app*.css*', 'js/app*.js*']);
 });
 
-gulp.task('build', ['minifyScripts', 'compileSass'], function() {
+gulp.task('build', ['minifyScripts', 'minifyCSS'], function() {
   return gulp.src(['css/app.css', 'js/app.js', 'index.html', 'img/**'], { base: './' })
              .pipe(gulp.dest('dist'));
 });
